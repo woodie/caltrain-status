b225fd28ea9d31c9d9e2a141f72ff3e1bff10fa3
John Woodell <jwoodell@chanzuckerberg.com>
2023-01-02 01:02:28 -0800

    Use twitter API

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "https://rubygems.org"
 
 gem "functions_framework", "~> 1.0"
-gem "nokogiri", "~> 1.13"
+gem "httparty"
--- app.rb
+++ app.rb
@@ -1,26 +1,21 @@
-require "functions_framework"
-require 'open-uri'
-require 'nokogiri'
 require 'time'
-
-URL = 'https://www.caltrain.com/alerts?active_tab=service_alerts_tab'
+require 'json'
+require 'httparty'
 
 FunctionsFramework.http("status") do |request|
-  begin
-    html = URI.open(URL)
-  rescue OpenURI::HTTPError => error
-    return 404
-  end
-  document = Nokogiri::HTML.parse(html)
-
   time_now = Time.now
+  bearer_token = ENV["BEARER_TOKEN"]
+  query = { "max_results" => 9, "tweet.fields" => "created_at" }
+  headers = { "Authorization" => "Bearer #{bearer_token}" }
+  endpoint_url = "https://api.twitter.com/2/users/919284817/tweets"
+
+  resp = HTTParty.get(endpoint_url, query: query, headers: headers)
+  return 421 if resp.code != 200
+
   response = {updates: []}
-  tweets = document.at('.view-tweets')
-  tweets.css('.views-row').each do |row|
-    time = row.at('time').attributes['datetime'].value
-    mins =  (time_now - Time.parse(time)).to_i
-    text = row.at('a').text
-    response[:updates] << {mins: mins, time: time, text: text}
+  JSON.parse(resp.body)['data'].each do |row|
+    mins = (time_now - Time.parse(row['created_at'])).to_i / 60
+    response[:updates] << {mins: mins, time: row['created_at'], text: row['text']}
   end
   response
 end
