c847786ca0134b63c832be65fba26ced8e47ad5e
John Woodell <jwoodell@chanzuckerberg.com>
2023-01-04 14:53:00 -0800

    Simplify response with one message.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "functions_framework", "~> 1.0"
+gem "standard", group: [:development, :test]
--- app.rb
+++ app.rb
@@ -1,45 +1,39 @@
-require 'time'
-require 'json'
-require 'net/http'
+require "time"
+require "json"
+require "net/http"
 require "functions_framework"
-
-MAX = 600
 
 FunctionsFramework.http("status") do |request|
   options_headers = {
-    "Access-Control-Allow-Origin"  => "*",
+    "Access-Control-Allow-Origin" => "*",
     "Access-Control-Allow-Methods" => "GET",
     "Access-Control-Allow-Headers" => "Content-Type",
-    "Access-Control-Max-Age"       => "3600"
+    "Access-Control-Max-Age" => "3600"
   }
   return [204, options_headers, []] if request.options?
 
-  bearer_token = ENV["BEARER_TOKEN"]
-  params = { "max_results" => 9, "tweet.fields" => "created_at" }
-  headers = { "Authorization" => "Bearer #{bearer_token}" }
+  api_params = {"max_results" => 20, "tweet.fields" => "created_at"}
+  api_headers = {"Authorization" => "Bearer #{ENV["BEARER_TOKEN"]}"}
   uri = URI("https://api.twitter.com/2/users/919284817/tweets")
-  uri.query = URI.encode_www_form(params)
-  response = Net::HTTP.get_response(uri, headers)
+  uri.query = URI.encode_www_form(api_params)
+  response = Net::HTTP.get_response(uri, api_headers)
   return [500, {}, ["Something went wrong."]] unless response.is_a?(Net::HTTPSuccess)
 
-  train = request.params['train'] || nil
-  time_now = Time.now
-  payload = {selected: "", updates: []}
-  JSON.parse(response.body)['data'].each do |row|
-    mins = (time_now - Time.parse(row['created_at'])).to_i / 60
-    parts = row['text'].split
-    payload[:updates] << {mins: mins, time: row['created_at'], text: row['text']}
-    if payload[:selected].empty? && mins < MAX && parts.size > 1 && parts[0] == 'Train' && parts[1] == train
-      payload[:selected] = row['text']
-    end
-  end
-  first = payload[:updates][0]
-  if payload[:selected].empty? && first[:mins] < MAX && !first[:text].start_with?('Train')
-    payload[:selected] = first[:text]
-  end
   cors_headers = {
     "Access-Control-Allow-Origin" => "*",
     "Content-type" => "application/json; charset=utf-8"
   }
-  [200, cors_headers, [payload.to_json]]
+  train_id = request.params["train"] || nil
+  fallback = ""
+  JSON.parse(response.body)["data"].each do |row|
+    break if (Time.now - Time.parse(row["created_at"])).to_i > 30000
+
+    parts = row["text"].split
+    if parts.size > 1 && parts[0] == "Train" && parts[1] == train_id
+      return [200, cors_headers, [{message: row["text"]}.to_json]]
+    elsif fallback.empty? && parts[0] != "Train"
+      fallback = row["text"]
+    end
+  end
+  [200, cors_headers, [{message: fallback}.to_json]]
 end
