8c1258e1a69be6520b4e7324b8a8da4101c04a54
John Woodell <jwoodell@chanzuckerberg.com>
2023-01-04 00:45:30 -0800

    Add a selected messages based on train.

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require 'json'
 require 'net/http'
 require "functions_framework"
 
+MAX = 600
+
 FunctionsFramework.http("status") do |request|
+  train = request.params['train'] || nil
   time_now = Time.now
   bearer_token = ENV["BEARER_TOKEN"]
   params = { "max_results" => 9, "tweet.fields" => "created_at" }
@@ -14,10 +17,18 @@ FunctionsFramework.http("status") do |request|
   response = Net::HTTP.get_response(uri, headers)
   return [500, {}, ["Something went wrong."]] unless response.is_a?(Net::HTTPSuccess)
 
-  payload = {updates: []}
+  payload = {selected: "", updates: []}
   JSON.parse(response.body)['data'].each do |row|
     mins = (time_now - Time.parse(row['created_at'])).to_i / 60
+    parts = row['text'].split
     payload[:updates] << {mins: mins, time: row['created_at'], text: row['text']}
+    if payload[:selected].empty? && mins < MAX && parts.size > 1 && parts[0] == 'Train' && parts[1] == train
+      payload[:selected] = row['text']
+    end
+  end
+  first = payload[:updates][0]
+  if payload[:selected].empty? && first[:mins] < MAX && !first[:text].start_with?('Train')
+    payload[:selected] = first[:text]
   end
   payload
 end
